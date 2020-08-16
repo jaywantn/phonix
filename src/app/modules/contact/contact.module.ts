fa6eac3910fedb70aec08e43b7dabbd71f5f72a8
gauravD292 <gauravd292@gmail.com>
2020-08-16 09:43:26 +0530

    added contact form

--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 // import { SharedModule } from 'shared/shared.module';
 import { ContactRoutes } from './contact-routing.module';
 import { ContactComponent } from './page/contact/contact.component';
-import { GoogleMapsModule } from '@angular/google-maps'
+import { GoogleMapsModule } from '@angular/google-maps';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ContactService } from './contact.service';
 @NgModule({
   declarations: [ContactComponent],
-  imports: [
-    CommonModule, ContactRoutes ,GoogleMapsModule
-  ],
+  imports: [CommonModule, ContactRoutes, GoogleMapsModule, ReactiveFormsModule],
   providers: [ContactService],
 })
-export class ContactModule { }
+export class ContactModule {}
--- src/app/modules/contact/page/contact/contact.component.ts
+++ src/app/modules/contact/page/contact/contact.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { ContactService } from '../../contact.service';
 
+import { FormGroup, FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-  details: any ;
+  details: any;
   zoom = 12;
-  center: google.maps.LatLngLiteral
+  center: google.maps.LatLngLiteral;
   options: google.maps.MapOptions = {
     mapTypeId: 'hybrid',
     zoomControl: false,
@@ -17,29 +19,40 @@ export class ContactComponent {
     disableDoubleClickZoom: true,
     maxZoom: 15,
     minZoom: 8,
-  }
-  constructor(private contactService: ContactService){}
+  };
+  constructor(private contactService: ContactService) {}
+
+  contactForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    message: new FormControl(),
+  });
+
   ngOnInit() {
-    navigator.geolocation.getCurrentPosition(position => {
+    navigator.geolocation.getCurrentPosition((position) => {
       this.center = {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
-      }
+      };
     });
     this.getDetails();
   }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+
   getDetails() {
-    this.contactService.getBanner().subscribe((data: any) => { 
+    this.contactService.getBanner().subscribe((data: any) => {
       this.details = data;
       console.log(this.details);
     });
   }
   zoomIn() {
-    if (this.zoom < this.options.maxZoom) this.zoom++
+    if (this.zoom < this.options.maxZoom) this.zoom++;
   }
 
   zoomOut() {
-    if (this.zoom > this.options.minZoom) this.zoom--
+    if (this.zoom > this.options.minZoom) this.zoom--;
   }
-
 }
