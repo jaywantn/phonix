c05d3f4ed8b3b425e96a60eceeda56a8461b2e45
Sidheshwar Udgire <saiudgire@gmail.com>
2020-10-06 20:47:31 +0530

    Gallery page added with api data

--- src/app/modules/pages/page/gallery/gallery.component.ts
+++ src/app/modules/pages/page/gallery/gallery.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Gallery, GalleryItem, ImageItem, ThumbnailsPosition, ImageSize } from '@ngx-gallery/core';
 import { PagesService } from '../../pages.service';
 import { Title, Meta } from '@angular/platform-browser';
+import { AppConstants } from 'src/app/app.constants';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
@@ -16,74 +17,51 @@ export class GalleryComponent implements OnInit {
     private pagesService: PagesService,
     public gallery: Gallery,
     private titleService: Title,
-    private meta: Meta) { }
+    private meta: Meta,
+    public appConstants: AppConstants) { }
 
-  ngOnInit() {
-    // 1. Create gallery items
-    this.items = data.map(item =>
-      new ImageItem({ src: item.srcUrl, thumb: item.previewUrl })
-    );
-
-    // Load items into the lightbox
-    this.basicLightboxExample();
-
-    // Load item into different lightbox instance
-    // With custom gallery config
-    this.withCustomGalleryConfig();
+  ngOnInit(): void {
     this.getGallery();
   }
-  getGallery(): any {
+  getGallery(): void {
     this.pagesService.getGallery().subscribe((data: any[]) => {
       this.galleryList = data;
       this.imageList = this.galleryList.galleryList;
+
+      // 1. Create gallery items
+      this.items = this.imageList.map(item =>
+        new ImageItem({
+            src: this.appConstants.bannerURL + 'gallery/' + item.g_img,
+            thumb: this.appConstants.bannerURL + 'gallery/' + item.g_img
+        })
+      );
+
+      this.imageList.map((item) => {
+        item.g_img = this.appConstants.bannerURL + 'gallery/' + item.g_img;
+        return item;
+      });
+
+      this.gallery.ref().load(this.items);
+      const lightboxGalleryRef = this.gallery.ref('anotherLightbox');
+
+      // (Optional) Set custom gallery config to this lightbox
+      lightboxGalleryRef.setConfig({
+        imageSize: ImageSize.Cover,
+        thumbPosition: ThumbnailsPosition.Top
+      });
+
+      // 3. Load the items into the lightbox
+      lightboxGalleryRef.load(this.items);
+
       this.seoDetails = this.galleryList.seo;
       this.seoGenerate();
-      console.log('getGallery', this.imageList);
     });
   }
+
   seoGenerate(): void {
     this.titleService.setTitle(this.seoDetails.seo_title);
     this.meta.updateTag({name: 'keywords', content: this.seoDetails.seo_keyword});
     this.meta.updateTag({name: 'description', content: this.seoDetails.seo_description}, 'name="description"');
   }
-  basicLightboxExample(): void {
-    this.gallery.ref().load(this.items);
-  }
-
-  /**
-   * Use custom gallery config with the lightbox
-   */
-  withCustomGalleryConfig(): void {
-
-    // 2. Get a lightbox gallery ref
-    const lightboxGalleryRef = this.gallery.ref('anotherLightbox');
-
-    // (Optional) Set custom gallery config to this lightbox
-    lightboxGalleryRef.setConfig({
-      imageSize: ImageSize.Cover,
-      thumbPosition: ThumbnailsPosition.Top
-    });
-
-    // 3. Load the items into the lightbox
-    lightboxGalleryRef.load(this.items);
-  }
 }
 
-const data = [
-  {
-    srcUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg',
-    previewUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg'
-  },
-  {
-    srcUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg',
-    previewUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg'
-  },
-  {
-    srcUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg',
-    previewUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg'
-  },
-  {
-    srcUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg',
-    previewUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg'
-  }
-];
